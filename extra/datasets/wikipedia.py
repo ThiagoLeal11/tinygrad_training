907b8d593ee7b73a5b7edb1513ccef298b49f51c
Thiago L. Pozati <thiagoleal11@gmail.com>
2024-01-11 12:39:09 -0300

    Scripts for downloading and loading the dataset
    The downloads script was copied from https://github.com/tinygrad/tinygrad/pull/2302
    
    The loading scripts was also spired on the same branch

--- extra/datasets/wikipedia.py
+++ extra/datasets/wikipedia.py
@@ -1,14 +1,16 @@
-from dataclasses import dataclass
 import itertools
+import queue
 import random
+import threading
+from dataclasses import dataclass
 from pathlib import Path
+from typing import Iterable, Generator, Optional
 
 import numpy as np
-from extra.tokenizer.bert import BertTokenizer
-from tinygrad import Tensor
-from tinygrad.helpers import getenv
-from typing import Iterable, Generator
+import tqdm
 
+from extra.tokenizer.bert import BertTokenizer
+from tinygrad.helpers import getenv
 
 DOCUMENT = list[list[str]]
 BASEDIR = Path(__file__).parent / "wiki"
@@ -26,7 +28,7 @@ class Instance:
     assert len(self.tokens) == len(self.segment_ids)
     assert len(self.masked_positions) == len(self.masked_labels)
 
-  
+
 def join_lists(lists: Iterable[list[str]]) -> list[str]:
   return list(itertools.chain.from_iterable(lists))
 
@@ -38,17 +40,10 @@ def concatenate_dict(dicts: list[dict]) -> dict:
   }
 
 
-def to_tensor(features: dict[str, np.ndarray]) -> dict[str, Tensor]:
-  return {
-    k: Tensor(v)  # TODO: Require_grads = False?
-    for k, v in features.items()
-  }
-
-
 def read_document(filepath: str, load_all: bool = False) -> Generator[DOCUMENT, None, None]:
   with open(filepath, "r", encoding="utf-8") as f:
     document = []
-    
+
     for line in f:
       line = line.strip()
       if line:
@@ -70,20 +65,21 @@ def read_all_document(filepath: str) -> list[DOCUMENT]:
 
     return [s for s in split_docs if s]
 
+
 def tokenize_doc(doc: DOCUMENT, tokenizer: BertTokenizer) -> DOCUMENT:
   return [tokenizer.tokenize(line) for line in doc]
 
 
 def select_from_document(doc: DOCUMENT, target_len: int, rand_gen, rand_start: bool, rand_end: bool) -> tuple[int, int]:
   start = 0 if not rand_start else rand_gen.randint(0, len(doc) - 1)
-  
+
   max_lines = 1
   for max_lines, _ in enumerate(doc[start:], start=1):
     size = sum(len(line) for line in doc[start:start + max_lines])
     if size >= target_len:
       break
-  
-  size = max_lines 
+
+  size = max_lines
   if rand_end and max_lines > 1:
     size = rand_gen.randint(1, max_lines - 1)
   return start, size
@@ -94,7 +90,7 @@ def truncate_seq_pair(a: list[str], b: list[str], max_num_tokens: int) -> tuple[
   if total_length <= max_num_tokens:
     return a, b
 
-  # Try to keep equal number of tokens on each side  
+  # Try to keep equal number of tokens on each side
   diff = total_length - max_num_tokens
   mid = max_num_tokens // 2
   a_end = len(a) - max(len(a) - mid, 0)
@@ -105,7 +101,7 @@ def truncate_seq_pair(a: list[str], b: list[str], max_num_tokens: int) -> tuple[
 
 def mask_tokens(tokens: list[str], vocab_words: list[str], rand_gen) -> tuple[list[str], list[int], list[str]]:
   num_to_predict = min(
-    getenv('MAX_PREDICTIONS_PER_SEQ', 20), 
+    getenv('MAX_PREDICTIONS_PER_SEQ', 20),
     max(1, int(round(len(tokens) * 0.15)))
   )
 
@@ -117,7 +113,7 @@ def mask_tokens(tokens: list[str], vocab_words: list[str], rand_gen) -> tuple[li
   for idx in valid_indexes:
     has_masked_enough = len(masked) >= num_to_predict
     if has_masked_enough:
-      break      
+      break
 
     if rand_gen.random() < 0.8:
       output[idx] = "[MASK]"
@@ -133,8 +129,9 @@ def mask_tokens(tokens: list[str], vocab_words: list[str], rand_gen) -> tuple[li
   return output, masked_pos, masked_labels
 
 
-def create_instance_from_document(rand_gen, doc: DOCUMENT, rand_doc: DOCUMENT, vocab_words: list[str]) -> Instance:
-  max_num_tokens = getenv('MAX_SEQ_LENGTH', 128) - 3 # [CLS] + 2 * [SEP]
+def create_instance_from_document(rand_gen, doc: DOCUMENT, rand_doc: DOCUMENT, vocab_words: list[str]) -> Optional[
+  Instance]:
+  max_num_tokens = getenv('MAX_SEQ_LENGTH', 128) - 3  # [CLS] + 2 * [SEP]
   target_seq_length = max_num_tokens
 
   if rand_gen.random() < 0.1:  # 10% of the time, pick a random sized document
@@ -142,7 +139,7 @@ def create_instance_from_document(rand_gen, doc: DOCUMENT, rand_doc: DOCUMENT, v
 
   _, a_size = select_from_document(doc, target_seq_length, rand_gen, False, True)
   a_tokens = join_lists(doc[:a_size])
-  
+
   # Next sentence from random document
   is_random_next = rand_doc != doc and (len(doc) == 1 or rand_gen.random() < 0.5)
   if is_random_next:
@@ -159,9 +156,6 @@ def create_instance_from_document(rand_gen, doc: DOCUMENT, rand_doc: DOCUMENT, v
   tokens = ["[CLS]"] + a + ["[SEP]"] + b + ["[SEP]"]
   segment_ids = [0] * (len(a) + 2) + [1] * (len(b) + 1)
 
-  assert len(a_tokens) >= 1
-  assert len(b_tokens) >= 1
-
   masked_tokens, masked_pos, masked_labels = mask_tokens(tokens, vocab_words, rand_gen)
 
   return Instance(
@@ -173,30 +167,33 @@ def create_instance_from_document(rand_gen, doc: DOCUMENT, rand_doc: DOCUMENT, v
   )
 
 
-def get_next_documents(filepaths: list[str]) -> Generator[list[DOCUMENT], None, None]:
-  opened_files = [read_all_document(f) for f in filepaths]
-  yield from opened_files
+def get_next_documents(filepaths: list[str], read_all: bool = False) -> Generator[list[DOCUMENT], None, None]:
+  if read_all:
+    opened_files = [read_all_document(f) for f in filepaths]
+    yield from opened_files
+    return
 
+  opened_files = [read_document(f) for f in filepaths]
+  has_docs = True
+  while has_docs:
+    current_docs = []
+    for f in opened_files:
+      try:
+        current_docs.append(next(f))
+      except StopIteration:
+        pass
 
-  # has_docs = True
-  # while has_docs:
-  #   current_docs = []
-  #   for f in opened_files:
-  #     try:
-  #       current_docs.append(next(f))
-  #     except StopIteration:
-  #       pass
-  #
-  #   has_docs = any(current_docs)
-  #   if has_docs:
-  #     yield current_docs
+    has_docs = any(current_docs)
+    if has_docs:
+      yield current_docs
 
 
 def instance_to_features(instance: Instance, tokenizer: BertTokenizer) -> dict:
   max_seq_len = getenv('MAX_SEQ_LENGTH', 128)
   mas_pred_len = getenv('MAX_PREDICTIONS_PER_SEQ', 20)
   assert len(instance.tokens) <= max_seq_len, f"instance length {len(instance.tokens)} exceeds max length {max_seq_len}"
-  assert len(instance.masked_labels) <= mas_pred_len, f"instance masked length {len(instance.masked_labels)} exceeds max length {mas_pred_len}"
+  assert len(
+    instance.masked_labels) <= mas_pred_len, f"instance masked length {len(instance.masked_labels)} exceeds max length {mas_pred_len}"
 
   # Padding
   pad_size = max_seq_len - len(instance.tokens)
@@ -225,13 +222,29 @@ def instance_to_features(instance: Instance, tokenizer: BertTokenizer) -> dict:
   }
 
 
-def generate_features(tokenizer, file_list: list[str]) -> Generator[tuple[dict, Instance], None, None]:
+# def parallel_read_tokenized(tokenizer, file_list: list[str]):
+
+
+def generate_features(tokenizer, file_list: list[str], read_all: bool = False, skip_n: int = 0) -> Generator[
+  tuple[dict, Instance], None, None]:
+  # TODO accumulate in a queue
   """Convert raw text to masked NSP samples"""
   rand_gen = random.Random(getenv('SEED', 12345))
   duplication_factor = getenv('DUP_FACTOR', 10)
   vocab_words = list(tokenizer.vocab.keys())
 
-  for docs in get_next_documents(file_list):
+  pbar = tqdm.tqdm(total=skip_n, disable=skip_n==0, desc='Skip dataset')
+
+  for docs in get_next_documents(file_list, read_all):
+    num_docs = len(docs) * duplication_factor
+    if skip_n > num_docs:
+      skip_n -= num_docs
+      pbar.update(num_docs)
+      continue
+    if skip_n:
+      pbar.update(skip_n)
+    pbar.close()
+
     docs = [[tokenizer.tokenize(line) for line in doc] for doc in docs]
     for _ in range(duplication_factor):
       docs_indexes = list(range(len(docs)))
@@ -241,36 +254,53 @@ def generate_features(tokenizer, file_list: list[str]) -> Generator[tuple[dict, 
         rand_idx = rand_gen.randint(0, len(docs) - 1)
         rand_doc = docs[rand_idx] if rand_idx != idx else docs[(rand_idx + 1) % len(docs)]
         instance = create_instance_from_document(rand_gen, docs[idx], rand_doc, vocab_words)
+        if not instance:
+          continue
         features = instance_to_features(instance, tokenizer)
         yield features, instance
 
 
-def load_dataset(bs: int, is_validation: bool = False) -> Generator[tuple[dict, Instance], None, None]:
+def buffer_to_queue(gen: Generator, buffer: queue.Queue):
+  while True:
+    try:
+      buffer.put(next(gen), block=True)
+    except StopIteration:
+      break
+
+
+def load_dataset(bs: int, is_validation: bool = False, skip_n: int = 0) -> Generator[tuple[dict, Instance], None, None]:
   files = [BASEDIR / f"results4/part-{part:05d}-of-00500" for part in range(500)]
-  
+
   if is_validation:
     files = [BASEDIR / "results4/eval.txt"]
 
+  buffer = queue.Queue(maxsize=1024)
   tokenizer = BertTokenizer(Path(__file__).parent / "wiki" / "vocab.txt", is_lower_case=True)
-  gen = generate_features(tokenizer, [str(f) for f in files])
+  gen = generate_features(tokenizer, [str(f) for f in files], skip_n=skip_n)
+  buffer_thread = threading.Thread(target=buffer_to_queue, args=(gen, buffer))
+  buffer_thread.start()
+
   while True:
     results = []
     for _ in range(bs):
-      # TODO: Should discard the last batch?
-      results.append(next(gen))
+      # TODO: Should discard the last batch if it's not full
+      results.append(buffer.get(block=True))
     features, instances = zip(*results)
 
-    yield to_tensor(concatenate_dict(features)), instances
+    yield concatenate_dict(features), instances
+
+  buffer_thread.join()
 
 
 if __name__ == "__main__":
   import time
+
   count = 0
 
   # TODO: colocar script pra baixar tudo e descompactar, se não existir
 
   st = time.time()
-  for X, Y in load_dataset(32, True):
+  for X, Y in load_dataset(32, False, skip_n=100_000):
     # print('batch', count, 'time', time.time() - st)
     count += 1
     st = time.time()
